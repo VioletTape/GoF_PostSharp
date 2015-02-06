6bf8ec935ff1a0232fee5b430f34ddeab872d540
VioletTape <my@violet-tape.net>
2015-02-06 21:04:33 +0300

    Facade Implemented

---
+++ FacadeExample/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FacadeExample {
+    internal class Program {
+        private static void Main(string[] args) {
+            // There is no reason and any possibility to use AOP for Facade pattern 
+            Console.WriteLine("There is no reason and any possibility to use AOP for Facade pattern ");
+        }
+    }
+}
